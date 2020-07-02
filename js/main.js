ff2c334762458daa289db434b61196f095d8fa84
Jose Guerra <jose.guerra@finalsite.com>
2020-07-02 14:18:15 +0100

    updated calculations

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const deg = 6,
+const deg = 6, // 360 deg / 60 deg = 6
       hourEl = document.getElementById('hr'),
       minutesEl = document.getElementById('mn'),
       secondsEl = document.getElementById('sc');
 
 const clock = () => {
   let day = new Date(),
-      hours = day.getHours() * 30,
-      minutes = day.getMinutes() * deg,
-      seconds = day.getSeconds() * deg;
+      seconds = day.getSeconds() * deg,
+      minutes = (day.getMinutes() * deg) + (seconds / 60),
+      hours = (day.getHours() * (360 / 12)) + ((day.getMinutes() * deg) / 12);
   
-  hourEl.style.transform = `rotateZ(${(hours) + (minutes / 12)}deg)`;
+  hourEl.style.transform = `rotateZ(${(hours)}deg)`;
   minutesEl.style.transform = `rotateZ(${minutes}deg)`;
   secondsEl.style.transform = `rotateZ(${seconds}deg)`;
 }
 
 clock();
 
-setInterval(clock);
+setInterval(clock, 1000);
