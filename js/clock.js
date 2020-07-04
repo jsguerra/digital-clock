fc2125a20fd45d9a477ce5a5c61b3026415c1e04
Jose Guerra <jose@gcostudios.com>
2020-07-04 11:18:37 +0100

    initial commit

---
+++ js/clock.js
@@ -0,0 +1,27 @@
+// Clock Elements
+const hourEl = document.getElementById('hr'),
+      minutesEl = document.getElementById('mn'),
+      secondsEl = document.getElementById('sc');
+
+// Clock Hand Rotation
+const clockRotate = (el, time) => {
+  el.style.transform = `rotateZ(${(time)}deg)`;
+}
+
+// Clock Setup and Equations
+const clock = () => {  
+  let day = new Date(),
+      seconds = day.getSeconds() * (360 / 60),
+      minutes = (day.getMinutes() * (360 / 60)) + (seconds / 60),
+      hours = (day.getHours() * (360 / 12)) + (day.getMinutes() * (360 / 60)) / 12;
+      
+  clockRotate(secondsEl, seconds);
+  clockRotate(minutesEl, minutes);
+  clockRotate(hourEl, hours);
+
+  window.requestAnimationFrame(clock);
+}
+
+// Init Clock
+clock();
+// setInterval(clock);
